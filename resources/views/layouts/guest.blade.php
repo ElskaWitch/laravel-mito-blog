147900fc6f14fb8cde83de272626314c29882ff2
ElskaWitch <justinelabas95@gmail.com>
2022-09-29 15:33:31 +0200

    dashbord

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+		<meta charset="utf-8">
+		<meta content="width=device-width, initial-scale=1" name="viewport">
+		<meta content="{{ csrf_token() }}" name="csrf-token">
+
+		<title>{{ config('app.name', 'Laravel') }}</title>
+
+		<!-- Fonts -->
+		<link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+		<!-- Scripts -->
+		@vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+		<div class="font-sans text-gray-900 antialiased">
+				{{ $slot }}
+		</div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', [PostController::class, 'index'])->name('home');
-Route::get('about', [PostController::class, 'about'])->name('about');
 Route::resource('posts', PostController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
